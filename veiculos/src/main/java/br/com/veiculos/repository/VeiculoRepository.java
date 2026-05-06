192425a92b493434f437f6779fc7da3a3ba7d7ec
Wellington Diego <4105222tom@gmail.com>
2026-05-06 09:57:38 -0300

    Criado o VeiculoRepository

---
+++ veiculos/src/main/java/br/com/veiculos/repository/VeiculoRepository.java
@@ -0,0 +1,8 @@
+package br.com.veiculos.repository;
+
+
+import br.com.veiculos.model.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+}
