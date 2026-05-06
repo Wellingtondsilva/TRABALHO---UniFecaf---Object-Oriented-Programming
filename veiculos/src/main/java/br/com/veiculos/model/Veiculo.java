cca7e646f7964ecf9e7b1c925c18b7528bb532d1
Wellington Diego <4105222tom@gmail.com>
2026-05-06 09:55:02 -0300

    Criado o model Veiculo

---
+++ veiculos/src/main/java/br/com/veiculos/model/Veiculo.java
@@ -0,0 +1,46 @@
+package br.com.veiculos.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String modelo;
+    private String marca;
+    private int ano;
+    private String cor;
+    private double preco;
+    private double quilometragem;
+    private String status;
+
+    public Veiculo() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
+    public String getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+
+    public int getAno() { return ano; }
+    public void setAno(int ano) { this.ano = ano; }
+
+    public String getCor() { return cor; }
+    public void setCor(String cor) { this.cor = cor; }
+
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
+
+    public double getQuilometragem() { return quilometragem; }
+    public void setQuilometragem(double quilometragem) { this.quilometragem = quilometragem; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
